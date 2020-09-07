970eada38506926750f22eab24b82578bad97728
saad277 <saadsheikh27@gmail.com>
2020-09-07 18:09:33 +0500

    complete later

---
+++ Firebase Storege/config.js
@@ -0,0 +1,12 @@
+ // Your web app's Firebase configuration
+ var firebaseConfig = {
+    apiKey: "AIzaSyCmeM4Kyk9giFFksxCIso7kEfozDNQCaqU",
+    authDomain: "native-firebase-6eb6c.firebaseapp.com",
+    databaseURL: "https://native-firebase-6eb6c.firebaseio.com",
+    projectId: "native-firebase-6eb6c",
+    storageBucket: "native-firebase-6eb6c.appspot.com",
+    messagingSenderId: "877280431920",
+    appId: "1:877280431920:web:cd151a69dcf23a50fc42f5"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
